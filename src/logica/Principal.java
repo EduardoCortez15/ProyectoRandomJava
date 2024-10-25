cab2cf3ad46ad6f5ba3fab82b864ed5bc69a0f2f
Cortez Jesus <u22241756@utp.edu.pe>
2024-10-25 14:50:34 -0500

    3er commit , se añadio cambio2

--- src/logica/Principal.java
+++ src/logica/Principal.java
@@ -8,6 +8,7 @@ public class Principal {
     public static void main(String[] args) {
         System.out.println("hola");
         System.out.println("cambio1");
+        System.out.println("cambio2");
     }
     
 }
